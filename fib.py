e0d62f59e4346c6d6082db2f737ef1f0be8d95f0
keymale <keisukeishiakwa7@gmail.com>
2020-08-31 22:36:32 +0900

    first commit

--- fib.py
+++ fib.py
@@ -1,10 +1,18 @@
 from typing import Dict
+from functools import lru_cache
 memo: Dict[int,int] = {0: 0, 1:1}# 基底部
 def fib(n):
     if n not in memo:
         memo[n] = fib(n - 1) + fib(n - 2)#memoliize
     return memo[n]
 
+@lru_cache(maxsize=None)
+def fib0(n):
+    if n < 2:
+        return n
+    return fib0(n - 1)+ fib0(n - 2)
+
+
 if __name__=="__main__":
     for i in range(10):
-        print(fib(i))
+        print(fib0(i))
