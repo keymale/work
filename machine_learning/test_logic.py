5d1a72dabcf52cb4894f80aeb6f4b3b2a369a3c9
keymale <keisukeishikawa7@gmail.com>
2020-10-20 01:53:51 +0900

    add back probagation and hidden layer

--- machine_learning/test_logic.py
+++ machine_learning/test_logic.py
@@ -11,6 +11,15 @@ def cross_entropy(y, t):
     L = (1 / N) * np.sum(-t * np.log(y) - (1 - t)*np.log(1 - y))
     return L
 
+
+def ReLU(u):
+    return np.where(u <= 0, 0, u)
+
+
+def dReLU(u):
+    return np.where(u <= 0, 0, 1)
+
+
 def dLdw(y, t, x):
     N = len(t)
     nn = len(x[0])
@@ -20,10 +29,33 @@ def dLdw(y, t, x):
     ans = (1 / N) * np.array(ans)
     return ans
 
+def dLdw_hidden(y, t, x, wj, u):
+    '''
+    u_s_j:s番目の入力ベクトルx‗sに対する、隠れ層j番目のニューロン活性
+    y_s_j:s番目の入力ベクトルx‗sに対する、隠れ層j番目のニューロン出力
+    w_j_i:入力層i番目のニューロンから隠れ層j番目のニューロンへのシナプスの重み
+    sigma_s = y_s - t_s
+    
+    '''
+    N = len(t)
+    nn = len(x[0])
+    ans = []
+    sigma_s = y - t
+    for i in range(nn):
+        ans.append(np.sum(sigma_s * wj * dReLU(u) * x[:,i]))
+    ans = (1 / N) * np.array(ans)
+    return ans
+
 
 def dLdb(y, t):
     N = len(t)
     return (1 / N)*np.sum(y - t)
+
+
+def dLdb_hidden(y, t, wj, u):
+    N = len(t)
+    sigma_s = y - t
+    return (1 / N)*np.sum(sigma_s * wj * dReLU(u))
 
 
 class neuron():
@@ -37,31 +69,60 @@ class neuron():
         for xx in self.x:
             self.u.append(np.dot(xx, self.w) + self.b)
         self.u = np.array(self.u)
-        self.y = sigmoid(self.u)
+        
+        return self.u
 
+    def spark_output(self, u):
+        self.y = sigmoid(self.u)
         return self.y
 
-    def rev_wb(self, t, eta):
+    def rev_wb(self, t, eta, alpha = 0.0001):
         self.t = t
-        self.w = self.w - (eta * dLdw(y = self.y, t = self.t, x = self.x))
+        self.w = (1 - eta * alpha) * self.w - (eta * dLdw(y = self.y, t = self.t, x = self.x))
         self.b = self.b - (eta * dLdb(y = self.y, t = self.t))        
+
+class hidden_neuron(neuron):
+    def spark_output(self, u):
+        self.y = ReLU(self.u)
+        return self.y
+
+    def rev_wb(self, t, eta, wj, alpha = 0.0001):
+        self.t = t
+        self.w = (1 - eta * alpha) * self.w - (eta * dLdw_hidden(self.y, self.t, self.x, wj, self.u))
+        self.b = self.b - (eta * dLdb_hidden(self.y, self.t, wj, self.u))
+
+
 
 if __name__ == "__main__":
     x = np.array(((0, 0),
          (0, 1),
          (1, 0),
          (1, 1)))
-    t = np.array((0, 0, 1, 1))
-    w = np.array((0.5, 0.5))
-    eta = 0.1
-    neuron1 = neuron(w)
+    t = np.array((0, 1, 1, 0))
+    hidden_num =12
+    neuron_hid = [[] for _ in range(hidden_num)]
+    u = [[] for _ in range(hidden_num)]
+    y = [[] for _ in range(hidden_num)]
+    eta = 0.001
+    for i in range(hidden_num):
+        neuron_hid[i] = hidden_neuron(np.random.randn(2))
+    neuron_out = neuron(np.random.randn(hidden_num))
+
     ans  = [[], [], [], []]
-    epoc = 1000
+    ans_u  = [[], [], [], []]
+    epoc = 20000
     for i in range(epoc):
-        y = neuron1.spark(x)
-        neuron1.rev_wb(t, eta)
+        for j in range(hidden_num):
+            u[j] = neuron_hid[j].spark(x)
+            y[j] = neuron_hid[j].spark_output(u[j])
+        uo = neuron_out.spark(np.stack([y[j]for j in range(hidden_num)], 1))
+        yo = neuron_out.spark_output(uo)
+        for j in range(hidden_num):
+            neuron_hid[j].rev_wb(t, eta, neuron_out.w[j])
+        neuron_out.rev_wb(t, eta)
         for i in range(4):
-            ans[i].append(y[i])
+            ans[i].append(yo[i])
+            ans_u[i].append(uo[i])
     cnt = list(range(epoc))
     plt.plot(cnt,ans[0],label = "0")
     plt.plot(cnt,ans[1],label = "1")
