b799e7baa2bcc2a4b505d17a89d4eb170b4c3a8d
keymale <keisukeishikawa7@gmail.com>
2020-10-30 01:22:24 +0900

    pygame reversi

--- pygame_test.py
+++ pygame_test.py
@@ -2,59 +2,151 @@
 import pygame
 from pygame.locals import *
 import time
+import numpy as np
 
-grid_num = 8
-grid = [[0 for _ in range(grid_num)] for _ in range(grid_num)]
+class Reversi():
+    def __init__(self,w, h, edge, grid_n):
+        self.w = w
+        self.h = h
+        self.edge = edge
+        self.grid_n = grid_n
+        self.w0 = self.w // self.grid_n
+        self.turn = "B"
+        self.nextturn = "W"
+        self.grid = np.array([['N' for _ in range(self.grid_n + 2)] for _ in range(self.grid_n + 2)])
+        pygame.init() # 初期化
+        self.font = pygame.font.Font(None, 55)
+        self.scr = pygame.display.set_mode((self.w, self.h)) 
+        pygame.display.set_caption("Pygame Test") 
+        self.grid[4][4] = "B"
+        self.grid[4][5] = "W"
+        self.grid[5][4] = "W"
+        self.grid[5][5] = "B"
+        self.update()
 
-
-def initialize(width, height, edge, grid):
-    pygame.init() # 初期化
-    screen = pygame.display.set_mode((width, height)) 
-    pygame.display.set_caption("Pygame Test") 
-    screen.fill((0,0,0,))
-    for w in range(grid_num):
-        for h in range(grid_num):
-            pygame.draw.rect(screen, (127,255,0), Rect(width // grid_num * w + edge, 
-                                                       width // grid_num * h + edge,
-                                                       width // grid_num - (2*edge), 
-                                                       width // grid_num - (2*edge)))
-    grid[3][3] = "B"
-    grid[3][4] = "W"
-    grid[4][3] = "W"
-    grid[4][4] = "B"
-    return screen
-
-
-def update(width, height, edge, grid, screen):
-    for w in range(grid_num):
-        for h in range(grid_num):
-            if grid[h][w] == "B":
-                pygame.draw.circle(screen, (5,5,5), (int(width//grid_num*(0.5 + w)),int(width//grid_num*(0.5 + h))),width // grid_num//2 - (edge))
-            if grid[h][w] == "W":
-                pygame.draw.circle(screen, (250,250,250), (int(width//grid_num*(0.5 + w)),int(width//grid_num*(0.5 + h))),width // grid_num//2 - (edge))
-                pygame.draw.circle(screen, (5,5,5), (int(width//grid_num*(0.5 + w)),int(width//grid_num*(0.5 + h))),width // grid_num//2 - (edge),1)
-
-def main():
-    width = 600
-    height = 700
-    edge = 1
-    screen = initialize(width, height, edge, grid)
-    while(True):
-        update(width, height, edge, grid, screen)
+    def cnt_piece(self):
+        self.cnt_b = np.count_nonzero(self.grid == "B")
+        self.cnt_w = np.count_nonzero(self.grid == "W")
+        self.cnt_text = self.font.render(str(self.cnt_b) + "(B)VS" + str(self.cnt_w) + "(W)", True, (255,255,255))
+        self.scr.blit(self.cnt_text,[self.w/2, self.w])
+ 
+    def update(self):
+        '''盤面更新'''
+        self.scr.fill((0,0,0,))
+        for wi in range(self.grid_n):
+            for hi in range(self.grid_n):
+                pygame.draw.rect(self.scr,
+                                 (127,255,0),
+                                 Rect(self.w0 * wi + self.edge, 
+                                      self.w0 * hi + self.edge,
+                                      self.w0 - (2 * self.edge), 
+                                      self.w0 - (2 * self.edge)))
+        for wi in range(self.grid_n + 2):
+            for hi in range(self.grid_n + 2):
+                if self.grid[hi][wi] == "B":
+                    pygame.draw.circle(self.scr, (5,5,5), (int(self.w0*(-0.5 + wi)), int(self.w0*(-0.5 + hi))), self.w0//2 - (self.edge))
+                if self.grid[hi][wi] == "W":
+                    pygame.draw.circle(self.scr, (250,250,250), (int(self.w0*(-0.5 + wi)),int(self.w0*(-0.5 + hi))),self.w0//2 - (self.edge))
+                    pygame.draw.circle(self.scr, (5,5,5), (int(self.w0*(-0.5 + wi)),int(self.w0*(-0.5 + hi))), self.w0//2 - (self.edge),1)
+        self.cnt_piece()
         pygame.display.update()
 
-        for event in pygame.event.get(): # 終了処理
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == MOUSEBUTTONDOWN and event.button == 1:
-                x, y = event.pos
-                for w in range(grid_num):
-                    for h in range(grid_num):
-                        if x > width // grid_num * w + edge and x < width // grid_num * (w + 1) + edge and y > width // grid_num * h + edge and y < width // grid_num * (h + 1) + edge:
-                            x_pos = w
-                            y_pos = h  
-                print(x_pos,y_pos)
+    def change_turn(self):
+        self.turn, self.nextturn = self.nextturn, self.turn
+
+    def put_enable(self):
+        self.en = []
+        def check(hi, wi, hi_cnt, wi_cnt):
+            hi_cnt0 = hi_cnt
+            wi_cnt0 = wi_cnt
+            if not self.grid[hi + hi_cnt][wi + wi_cnt] == self.nextturn:
+                return False
+            else:
+                while True:
+                    hi_cnt0 += hi_cnt
+                    wi_cnt0 += wi_cnt
+                    if self.grid[hi + hi_cnt0][wi + wi_cnt0] == "N":
+                        return False
+                    if self.grid[hi + hi_cnt0][wi + wi_cnt0] == self.turn:
+                        self.en.append([hi,wi])
+                        return True
+        for wi in range(1,self.grid_n + 1):
+            for hi in range(1,self.grid_n + 1):
+                if self.grid[hi][wi] == "N":
+                    for hi_c in [-1, 0, 1]:
+                        for wi_c in [-1, 0, 1]:
+                            if not(hi_c == 0 and wi_c == 0): 
+                                if check(hi, wi, hi_c, wi_c):
+                                    break
+                        else:
+                            continue
+                        break
+        if self.en == []:
+            print("置けるとこないで")
+            return False
+        else:
+            return True
+
+    def return_piece(self, w, h):
+        change_piece = []
+        self.grid[h][w] = self.turn
+        for hi_c in [-1, 0, 1]:
+            for wi_c in [-1, 0, 1]:
+                hi_c0 = 0
+                wi_c0 = 0
+                while True:
+                    hi_c0 += hi_c
+                    wi_c0 += wi_c
+                    if self.grid[h + hi_c0][w + wi_c0] == self.nextturn:
+                        change_piece.append([h + hi_c0, w + wi_c0])
+                        print(change_piece)
+                    elif self.grid[h + hi_c0][w + wi_c0] == self.turn:
+                        if change_piece == []:
+                            break
+                        else:
+                            for hc, wc in change_piece:
+                                self.grid[hc][wc] = self.turn
+                            break
+                    else:
+                        change_piece = []
+                        break
+
+    def click2pos(self, click):
+        for pos in range(self.grid_n):
+            if 0 < click - self.edge -self.w0 * pos < self.w0:
+                return pos + 1
+    
+    def play(self):
+        while(True):
+            for event in pygame.event.get(): # 終了処理
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == MOUSEBUTTONDOWN and event.button == 1:
+                    x, y = event.pos
+                    x_pos = self.click2pos(x)
+                    y_pos = self.click2pos(y)
+                    print("get_posision")
+                    if self.put_enable():
+                        print("置けます")
+                        if [y_pos, x_pos] in self.en:
+                            self.return_piece(x_pos, y_pos)
+                    else:
+                        print("change turn")
+                        print(self.turn)
+                    self.change_turn()
+                    self.update()
+
+
+
+def main():
+    w = 600
+    h = 700
+    edge = 1
+    grid_n = 8
+    reversi = Reversi(w, h, edge, grid_n)
+    reversi.play()
+    
 
 def test():
     width = 600
@@ -94,5 +186,5 @@ def test():
                 sys.exit()
         
 if __name__ == "__main__": 
-    #main()
-    test()
+    main()
+    #test()
